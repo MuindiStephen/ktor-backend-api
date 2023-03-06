9e871c63ded7e5d858baae880179e839bc58aca7
MuindiStephen <stephenmuindi241@gmail.com>
2023-03-06 14:11:15 +0300

    [fixes] invalidated caches

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,12 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
+    application
     kotlin("jvm") version "1.8.10"
-    id("io.ktor.plugin") version "2.2.4"
+    //id("io.ktor.plugin") //version "2.2.4"
 }
 
-group = "com.example"
+group = "com.steve_md"
 version = "0.0.1"
 application {
     mainClass.set("com.example.ApplicationKt")
--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,18 +4,9 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
-import com.sun.xml.internal.ws.client.ContentNegotiation
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
-        install(ContentNegotiation) {
-            json(Json {
-                prettyPrint = true
-                isLenient
-            })
-        }
-        configureRouting()
-    }
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
