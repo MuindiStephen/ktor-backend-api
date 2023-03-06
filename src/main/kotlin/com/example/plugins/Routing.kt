1b5562af129edb8af843aeaab6622688e3404932
MuindiStephen <stephenmuindi241@gmail.com>
2023-03-06 16:24:15 +0300

    added order routes

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,6 +1,7 @@
 package com.example.plugins
 
 import com.example.routes.customerRouting
+import com.example.routes.listOrdersRoute
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
@@ -8,5 +9,6 @@ import io.ktor.server.application.*
 fun Application.configureRouting() {
     routing {
        customerRouting()  // To Register routes
+        listOrdersRoute()
     }
 }
