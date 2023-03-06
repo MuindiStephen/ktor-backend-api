1c003978d0a212ddaa1c19f54803b3c0510fcf11
MuindiStephen <stephenmuindi241@gmail.com>
2023-03-06 16:32:01 +0300

    [TASK] automated testing

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,14 +1,19 @@
 package com.example.plugins
 
 import com.example.routes.customerRouting
+import com.example.routes.getOrderRoute
 import com.example.routes.listOrdersRoute
+import com.example.routes.totalizeOrderRoute
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
 
 fun Application.configureRouting() {
     routing {
-       customerRouting()  // To Register routes
+        // To Register the routes
+       customerRouting()
         listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
     }
 }
